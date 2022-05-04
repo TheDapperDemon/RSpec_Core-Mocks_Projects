eaae00a93e4271d0a7186b0ddd8fb3931ff58d0b
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-05-04 15:23:15 -0600

    created before_each_spec.rb and before_all_spec.rb

---
+++ Core3.8/spec/before_each_spec.rb
@@ -0,0 +1,23 @@
+# require "rspec/expectations"
+#
+# class Thing
+#   def widgets
+#     @widgets ||= []
+#   end
+# end
+# describe Thing do
+#   before(:each) do
+#     @thing = Thing.new
+#   end
+#   describe "inizialized in before (:each)" do
+#     it "has 0 widgets" do
+#       @thing.should have(0).widgets
+#     end
+#     it "can get accept new widgets" do
+#       @thing.widgets << Object.new
+#     end
+#     it "does not share state across examples" do
+#       @thing.should have(0).widgets
+#     end
+#   end
+# end
