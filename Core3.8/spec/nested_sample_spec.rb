cb7715b0b0416a93b2f6083765e9a6a088314370
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-05-04 12:48:16 -0600

    Created Nested group in nested_sample_spec.rb

---
+++ Core3.8/spec/nested_sample_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "something" do
+  context "in one context" do
+    it "it does one thing" do
+    end
+  end
+  context "in another context" do
+    it "it does another thing" do
+    end
+  end
+end
